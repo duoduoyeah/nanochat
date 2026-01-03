42bf988c650a5a8b37fa63311dec14254bc40d34
Duoduo Yeah <lizhicuo2020@gmail.com>
2026-01-02 21:23:59 -0800

    debug pdlm config

--- nanochat/pdlm.py
+++ nanochat/pdlm.py
@@ -27,7 +27,7 @@ class PDLMConfig:
     
     is_causal: bool = True
     # need for training
-    model_name = None
+    model_name: str = "pdlm"
     prefix_pure_tokens: int = 0 
     all_vocab_size: int = -1
     mask_token_id: int = -1
